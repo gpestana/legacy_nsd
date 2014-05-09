605a20ecebd13cd9493b78ce031b490d5ce9cb21
gpestana <g6pestana@gmail.com>
2014-05-09 11:08:51 +0300

    populate and db

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-var fetchData = require('./lib/fetchData.js')
+var populateDB = require('./lib/populateDB.js')
 var db = require('./lib/db.js');
 var search = require('./lib/search.js');
 var server = require('./lib/server.js')
 
 var input = process.argv.slice(2);
 
-if (input == '-fetch') {
-	fetchData.fetchDocsNode(function(data) {
-		db.setNodeDocsJSON(data);
-		console.log("-- Official docs fetched successfuly");
-
-		fetchData.fetchPoweredContent(function(data) {
-			db.setUsersContent(data)
-			console.log("-- Users content fetched successfuly");
-
-			init_server();
-
-		});
+if (input == '-populate') {
+	populateDB.init(function() {
+		init_server();
 	});
 } else {
 	init_server();
@@ -27,8 +18,9 @@ if (input == '-fetch') {
 
 function init_server() {
 
-	fetchData.fetchIndex(function(data) {
-		console.log(data);
-	});
+	console.log("procceed...");
+	server.initServer();
+
+
 }
 
